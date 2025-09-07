7f121f68dbd8a27a632e2148802a7848cdb41814
Faisal Khan <afaisalkhan30@gmail.com>
2025-09-07 23:55:14 +0530

    change in app.jsx folder of client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
